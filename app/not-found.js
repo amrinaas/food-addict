ec417c08a1bb4f3fbbbe38a138329b00aa2af951
amrinaas <amrinaas96@gmail.com>
2024-05-06 10:22:39 +0700

    Add error and not found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>The page you are looking for is not found</h1>
+      <p>Please check your path!</p>
+    </div>
+  );
+}
