fe9010165eb3c88ff6963a0ab754516bf9acf9b6
amrinaas <amrinaas96@gmail.com>
2024-05-09 08:21:12 +0700

    Details meal page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,12 +1,37 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 export default function MealsDetailPage({ params }) {
+  const meal = getMeal(params.slug);
+  
+  if (!meal) {
+    notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
     <div>
-      <h1>This is {params.slug} route</h1>
-      <Link href="../">
-        <button>back</button>
-      </Link>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by: <a href={`mailto:${meal.creator}`}>{meal.creator}</a>
+          </p>
+          <p>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </div>
   );
 }
