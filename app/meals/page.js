8cf41e788dbb975c69e9d437a2a7a99130c2b032
amrinaas <amrinaas96@gmail.com>
2024-05-11 21:15:32 +0700

    Adding static metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -6,6 +6,12 @@ import classes from './page.module.css';
 import MealsGrid from '../components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+// Adding static metadata
+export const metadata = {
+  title: 'Meals',
+  description: 'Find any meal from all araound the world',
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
